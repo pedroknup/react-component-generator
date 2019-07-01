0e47d7a377afa4a0978a5b4e97b7d1ede3f433ce
Pedro Knup <phknup@gmail.com>
2019-07-01 13:45:35 +0200

    fix: fixed storybook and test genereator

--- src/generate-files.ts
+++ src/generate-files.ts
@@ -20,25 +20,25 @@ const ${componentName}Component = (props: I${componentName}ComponentProps) => {
 export { ${componentName}Component };
   `;
 };
-export const generateTestFile = (fileName?: string, componentName?: string) => {
+export const generateStoryBookDocumentationFile = (fileName?: string, componentName?: string) => {
   return `
 import * as React from "react";
 
 import { storiesOf } from "@storybook/react";
 import { withA11y } from "@storybook/addon-a11y";
-//import { withProvider } from "@app/modules/core/storybook-provider";
+import { withProvider } from "@app/modules/core/storybook-provider";
 import { ${componentName}Component } from "./${fileName}.component";
 
 storiesOf("${componentName}", module)
   .addDecorator(withA11y)
-  //.addDecorator(withProvider)
-  //.add("Basic implementation", () => (
-  //  <${componentName}Component />
-  //));
+  .addDecorator(withProvider)
+  .add("Basic implementation", () => (
+    <${componentName}Component />
+  ));
 
   `;
 };
-export const generateStoryBookDocumentationFile = (fileName?: string, componentName?: string) => {
+export const generateTestFile = (fileName?: string, componentName?: string) => {
   return `
 import React from "react";
 import { shallow } from "enzyme";
